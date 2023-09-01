61dfb0b2bfb4e85bec2c31ec94d75bfaa8610f30
zhoub <zhoubing19820720@hotmail.com>
2023-09-01 15:11:12 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QMainWindow>
-//#include "ui_untitled.h"
+#include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-//    QMainWindow w;
-//    Ui_MainWindow uiMainWindow;
-//    uiMainWindow.setupUi(&w);
-//    w.show();
+//    QPushButton button("Hello world!", nullptr);
+//    button.resize(200, 100);
+//    button.show();
+    MainWindow uiMainWindow;
+    uiMainWindow.show();
     return QApplication::exec();
 }
